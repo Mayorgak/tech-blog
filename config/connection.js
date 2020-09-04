c277855d50a4e34c8415d9dc4b55f75b5bab0567
Kristen M <kristenmayorga@gmail.com>
2020-09-04 14:30:44 -0400

    setting up server

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,16 @@
-const Sequelize = require("sequelize");
-
-
 require("dotenv").config();
 
-const sequelize = new Sequelize("just_tech_news_db", "username", "password", {
-  host: "localhost",
-  dialect: "mysql",
-  port: 3306,
-});
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PW,
+  {
+    host: "localhost",
+    dialect: "mysql",
+    port: 3306,
+  }
+);
 
 module.exports = sequelize;
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const routes = require("./controllers");
+const routes = require("./routes");
 const sequelize = require("./config/connection");
 const path = require("path");
 
@@ -15,7 +15,8 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
+// turn on routes
+app.use(routes);
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
