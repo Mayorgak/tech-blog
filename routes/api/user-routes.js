a8412e4ffd1b98f4d492b973e4bf7ffa55e28c76
Kristen M <kristenmayorga@gmail.com>
2020-09-04 22:52:43 -0400

    Check user password

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -70,35 +70,34 @@ router.get('/:id', (req, res) => {
       });
   });
 
-
+// Find User's account
 router.post('/login', (req, res) => {
   // expects {email: 'lernantino@gmail.com', password: 'password1234'}
   User.findOne({
     where: {
       email: req.body.email
     }
-  }).then(dbUserData => {
+    
+  }).then((dbUserData) => {
     if (!dbUserData) {
-      res.status(400).json({ message: 'No user with that email address!' });
+      res.status(400).json({ message: "No user with that email address!" });
       return;
     }
 
+
+    // Verify user
     const validPassword = dbUserData.checkPassword(req.body.password);
-
     if (!validPassword) {
-      res.status(400).json({ message: 'Incorrect password!' });
-      return;
-    }
+  res.status(400).json({ message: 'Incorrect password!' });
+  return;
+}
 
-    req.session.save(() => {
-      req.session.user_id = dbUserData.id;
-      req.session.username = dbUserData.username;
-      req.session.loggedIn = true;
-  
-      res.json({ user: dbUserData, message: 'You are now logged in!' });
-    });
+  res.json({ user: dbUserData, message: 'You are now logged in!' });
+
   });
 });
+
+//Logout User
 
 router.post('/logout', (req, res) => {
   if (req.session.loggedIn) {
@@ -111,6 +110,8 @@ router.post('/logout', (req, res) => {
   }
 });
 
+
+//Update User
 router.put('/:id', (req, res) => {
   
   User.update(req.body, {
@@ -132,6 +133,8 @@ router.put('/:id', (req, res) => {
     });
 });
 
+
+// Delete User
 router.delete('/:id', (req, res) => {
   User.destroy({
     where: {
